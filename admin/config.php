2ee30a819fe88527b18a66f7c836b2f880c16e7b
Sergey Telepnev <s.telepnev@kpresort.ru>
2023-10-23 17:13:48 +0300

    init

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://oc2.3.org/admin/');
+define('HTTP_CATALOG', 'http://oc2.3.org/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://oc2.3.org/admin/');
+define('HTTPS_CATALOG', 'http://oc2.3.org/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OSPanel/domains/OC2/admin/');
+define('DIR_SYSTEM', 'C:/OSPanel/domains/OC2/system/');
+define('DIR_IMAGE', 'C:/OSPanel/domains/OC2/image/');
+define('DIR_LANGUAGE', 'C:/OSPanel/domains/OC2/admin/language/');
+define('DIR_TEMPLATE', 'C:/OSPanel/domains/OC2/admin/view/template/');
+define('DIR_CONFIG', 'C:/OSPanel/domains/OC2/system/config/');
+define('DIR_CACHE', 'C:/OSPanel/domains/OC2/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OSPanel/domains/OC2/system/storage/download/');
+define('DIR_LOGS', 'C:/OSPanel/domains/OC2/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OSPanel/domains/OC2/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OSPanel/domains/OC2/system/storage/upload/');
+define('DIR_CATALOG', 'C:/OSPanel/domains/OC2/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'OC2.3');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
